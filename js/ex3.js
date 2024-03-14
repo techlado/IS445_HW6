54dc607bd4ca3268fd85f1f092c1f549177517ec
Danielle J. González <74265206+techlado@users.noreply.github.com>
2024-03-14 15:42:53 -0700

    Delete js directory

--- js/ex3.js
+++
@@ -1,8 +0,0 @@
-const styleParagraphs = () => {
-  const paragraphs = document.querySelectorAll('p');
-  paragraphs.forEach(paragraph => {
-    paragraph.style.fontWeight = 'bold';
-    paragraph.style.backgroundColor = 'yellow';
-});
-}
-styleParagraphs();
