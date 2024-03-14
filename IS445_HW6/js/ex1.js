da5fcbfad950c3fe375c3ac5b9881fd10ae9a069
Danielle J. González <74265206+techlado@users.noreply.github.com>
2024-03-14 15:13:34 -0700

    Delete IS445_HW6 directory

--- IS445_HW6/js/ex1.js
+++
@@ -1,7 +0,0 @@
-//Complete the following functions
-//Displays all body nodes
-const bodyNodes = () => {
-  const body = document.querySelector("body"); // Grab the first body tag
-  console.log(body); 
-}
-bodyNodes();
