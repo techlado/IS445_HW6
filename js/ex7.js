495bc1d3c56700d676f1713b534f5d361b8a7116
Danielle J. González <74265206+techlado@users.noreply.github.com>
2024-03-14 17:24:22 -0700

    Fixed ex7 to reflect desired output

--- js/ex7.js
+++ js/ex7.js
@@ -4,7 +4,7 @@ let num1 = parseInt(userInput1);
 userInput2 = prompt("Please enter a number: ");
 let num2 = parseInt(userInput2);
 
-if (isNaN(num1) || isNaN(num2) || !Number.isInteger(userInput1) || !Number.isInteger(userInput2)) {
+if (isNaN(num1) || isNaN(num2) || !Number.isInteger(Number(userInput1)) || !Number.isInteger(Number(userInput2))) {
   document.getElementById("content").innerHTML = '<span style="color:red; font-weight:bold;">' + 'Error!' + '</span>';
   
   document.getElementById("content").innerHTML += '<span>' + ' You must enter integers. You entered "' + userInput1 + '" and "' + userInput2 + '". Try again.' + '</span>';
@@ -12,7 +12,5 @@ if (isNaN(num1) || isNaN(num2) || !Number.isInteger(userInput1) || !Number.isInt
 } 
 else {
   var sum = num1 + num2;
-  document.getElementById("content").innerHTML = 'Sum of ' + num1 + ' and ' + num2 + ' is: <span style="color:red; font-weight:bold;">' + sum + '</span>';
+  document.getElementById("content").innerHTML = num1 + ' + ' + num2 + ' = <span style="color:red; font-weight:bold;">' + sum + '</span>';
 }
-
-console.log("test");
