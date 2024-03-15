cb2034b3099a76e39017210740d3af3dc620d13b
Danielle J. González <74265206+techlado@users.noreply.github.com>
2024-03-14 17:31:45 -0700

    Update ex1

--- js/ex1.js
+++ js/ex1.js
@@ -1,7 +1,10 @@
 //Complete the following functions
 //Displays all body nodes
 const bodyNodes = () => {
-  const body = document.querySelector("body"); // Grab the first body tag
-  console.log(body); 
+  const body = Array(document.querySelector("body"));
+  for (i = 0; i < body.length; i++) {
+    console.log(body[i]);
+  };
+  //  console.log(document.body.childNodes);
 }
 bodyNodes();
